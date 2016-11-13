653a1daa07ef5f74eac80afdc94417cab54ae413
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-11-12 20:30:30 -0600

    clean

---
+++ App/Panel/Http/Middleware/RouteServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace App\Panel\Providers;
+
+use Melisa\Laravel\Providers\RouteServiceProvider as RouteService;
+
+class RouteServiceProvider extends RouteService
+{
+    
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Panel\Http\Controllers';
+    
+}
