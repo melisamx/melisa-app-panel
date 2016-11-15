c9c83aa3950218cd7022316ecc1c323a43b7cd39
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-11-15 13:29:45 -0600

    update gitignore

--- App/Panel/routes/web.php
+++ App/Panel/routes/web.php
@@ -1,14 +1,9 @@
-<?php
+<?php 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+Route::get('/', 'HomeController@index')->middleware('auth');
 
-Route::get('/', 'HomeController@index')/*->middleware('auth')*/;
+Route::get('/login', function() {
+    
+    return redirect('../login');
+    
+});
