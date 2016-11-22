d58fd0ce9c322de5d097704816891871508e02cd
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-11-22 17:09:34 -0600

    add options

---
+++ App/Panel/resources/views/sencha/view/TreeOptions.js
@@ -0,0 +1,42 @@
+
+Ext.define('Melisa.panel.view.TreeOptions', {
+    extend: 'Ext.list.Tree',
+    
+    requires: [
+        'Melisa.panel.view.TreeOptionsController'
+    ],
+        
+    controller: 'apppaneltreeoptions',
+    alias: 'widget.apppaneltreeoptions',
+    ui: 'navigation',
+    defaults: {
+        indent: 15
+    },
+    
+    store: Ext.create('Ext.data.TreeStore', {
+        root: {
+            expanded: true,
+            children: [
+                {
+                    text: 'Security',
+                    iconCls: 'fa x-fa fa-comment',
+                    my: 'Melisa.chat.view.phone.Main',
+                    leaf: true
+                },
+                {
+                    text: 'System',
+                    iconCls: 'fa x-fa fa-folder',
+                    my: 'Melisa.drive.view.phone.Main',
+                    leaf: true
+                },
+                {
+                    text: 'Binacle',
+                    iconCls: 'fa x-fa fa-picture-o',
+                    my: 'Melisa.previos.view.phone.Main',
+                    leaf: true
+                }
+            ]
+        }
+    })
+        
+});
