4eef1a5802765d27dd819bac50daf496fcf05e2f
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-02-13 12:11:24 -0600

    update version app

--- App/Panel/Database/Seeds/ApplicationSeeder.php
+++ App/Panel/Database/Seeds/ApplicationSeeder.php
@@ -18,7 +18,7 @@ class ApplicationSeeder extends InstallSeeder
             'description'=>'Application Panel',
             'nameSpace'=>'Melisa.panel',
             'typeSecurity'=>'arat',
-            'version'=>'1.0.2',
+            'version'=>'1.0.3',
         ]);
         
     }
